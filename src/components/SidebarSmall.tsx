8f55d5550bf2a64aae38f84d6113b7fde9540707
abundance <abundanceanyanwu@gmail.com>
2023-12-12 16:31:47 +0100

    added mobile view

---
+++ src/components/SidebarSmall.tsx
@@ -0,0 +1,9 @@
+import { Fa500Px } from "react-icons/fa";
+import { MdMenu } from "react-icons/md";
+export default function () {
+  return (
+    <div className="w-screen h-[10%] bg-[#131810] border-r-[0.5px] border-[#464545] flex py-2 items-center justify-end text-3xl px-2 cursor-pointer">
+      <MdMenu />
+    </div>
+  );
+}
